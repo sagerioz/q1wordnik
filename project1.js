9cec85b0747105187cfe74ec8186bb464de31fdf
sagerioz <ericaepperson27@yahoo.com>
2017-03-03 09:31:38 -0700

    more

--- project1.js
+++ project1.js
@@ -9,19 +9,19 @@ $(document).ready(function() {
 
 function init() {
 
-if(localStorage.getItem("favGenre") === null){
-  localStorage.setItem("favGenre", JSON.stringify(favArr));
-  console.log(favoriteGenre);
+  if (localStorage.getItem("favGenre") === null) {
+    localStorage.setItem("favGenre", JSON.stringify(favArr));
+
   // let = document.getElementById("items");
   // let li = document.createElement("li");
   // li.innerHTML = favoriteGenre;
   // ul.appendChild(li);
+  }
+  else {
+    let favoriteGenre = JSON.parse(localStorage.getItem("favGenre"));
+  }
 
-}else{
-
-  let favoriteGenre = JSON.parse(localStorage.getItem("favGenre"));
-}
-return favoriteGenre
+  return favoriteGenre
 }
 
 
